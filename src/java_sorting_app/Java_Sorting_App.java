dc0bf4865ec2fba5bbead8b4bc19733376a49df5
António José Gomes <ajose.gomes@ajgomes.datanet-pt.net>
2016-12-30 01:51:14 +0000

    20161230.0150

--- src/java_sorting_app/Java_Sorting_App.java
+++ src/java_sorting_app/Java_Sorting_App.java
@@ -1,4 +1,4 @@
-/*
+/**
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,6 +6,7 @@
 package java_sorting_app ;
 
 import  java_sorting_app.number_rack ;
+import  java_sorting_app.sorter.* ;
 
 
 /**
@@ -21,6 +22,15 @@ public class Java_Sorting_App
     public static void main( String[] args )
     {
         // TODO code application logic here
+        
+        System.out.println( java_sorting_app.sorter.sort_type.BUBBLE_SORT.name() ) ;
+        System.out.println( java_sorting_app.sorter.sort_type.BUBBLE_SORT ) ;
+        sort_factory factory = new java_sorting_app.sorter.sort_factory() ;
+        String[] sort_algo_list = factory.get_sort_algorithm_list() ;
+        for( int i = 0 ; i < sort_algo_list.length ; i++ )
+        {
+            System.out.println( "Algorithm: " + sort_algo_list[ i ] ) ;
+        }
     }
     
 }
