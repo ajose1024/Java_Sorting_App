621ca83d36bd09c419278f3dd611f607d4adef3e
António José Gomes <ajose.gomes@ajgomes.datanet-pt.net>
2016-12-30 20:16:20 +0000

    20161230.2015 Adition of the sort_order ENUM

---
+++ src/java_sorting_app/sorter/sort_order.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_sorting_app.sorter ;
+
+/**
+ * ENUM with the sorting order for the "sorting algorithms"
+ */
+public enum sort_order
+{
+    ASCENDING ,
+    DESCENDING
+}
