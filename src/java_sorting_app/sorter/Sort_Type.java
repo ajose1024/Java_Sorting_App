de45e1d0b8f08780e0ef44f99d44df12333e6594
António José Gomes <ajose.gomes@ajgomes.datanet-pt.net>
2017-01-01 20:00:08 +0000

    20170101.1957 Code refactoring and improvment

---
+++ src/java_sorting_app/sorter/Sort_Type.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_sorting_app.sorter ;
+
+/**
+ * ENUM with the names of the "sorting algorithms"
+ */
+public enum Sort_Type
+{
+    BUBBLE_SORT ,
+    SEQUENTIAL_SORT
+}
